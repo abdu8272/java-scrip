9970edf7cca0b7f5bc1d5429a394e19c1a36e9be
abdu <abdu>
2024-05-27 14:34:17 +0500

    Java script

---
+++ script.js
@@ -0,0 +1,22 @@
+let box = +prompt("Son kiriting");
+alert("Natijani korish uchun consolni oching");
+for (let i = 1; i < 5; i++) {
+  box = box * i;
+  console.log(box);
+}
+    
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
